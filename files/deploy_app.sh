69975813f38aabda7675ab160bcde9186c937a07
C-Moore21 <camden.b.moore@gmail.com>
2020-08-24 12:14:20 -0600

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. Is your cat constantly stomping around driving you crazy? Is your cat clawing at your furniture? Think there's no answer? You're so stupid! There is! Kitten Mittens. Finally, there is an elegant, comfortable mitten for cats.... I couldn't hear anything! Is your cat one-legged? Is your cat fat, skinny, or an in-between? That doesn't matter! Cause one size fits all! Kitten Mittens! You'll be smitten! So come on down to Paddy's Pub. We're the hoooooooommee of the original Kitten Mittens. Meeeeeeeeeeowwwww!.
   <!-- END -->
 
   </div>
